7e7c47ddaf7a5dae74cbce4d4c6819a95b6cddc5
Toosick <preston_tighe@yahoo.com>
2016-12-13 02:05:37 -0600

    TaskManager conflict

--- FinalProject/TaskManager.swift
+++ FinalProject/TaskManager.swift
@@ -9,16 +9,6 @@
 import Foundation
 
 class  TaskManager {
-<<<<<<< HEAD
-//    static let taskNames = ["Button", "Slide", "Swipe", "FindNorth", "FlatSurface", "HandOverCamera", "Walk", "Shake", "Switch", "DoMath", "DifferentShade", "Clap", "PinchScreen", "FindAFriend"]
-    
-    //Here for testing purposes
-    static let prestonTaskNames = ["Button", "Slide", "Swipe", "FlatSurface", "HandOverCamera", "Walk", "Shake", "Switch", "Clap"]
-    static let jennTaskNames = ["DoMath", "DifferentShade", "FindNorth", "PinchScreen", "FindAFriend"]
-    
-    //Change this to the task you are testing
-    static let taskNames = ["FindAFriend"]
-=======
 
 //    static let taskNames = ["Button", "Slide", "Swipe", "FlatSurface", "HandOverCamera", "Walk", "Shake", "Switch", "DoMath", "DifferentShade", "Clap", "PinchScreen", "FindAFriend", "FindNorth", "FingerDown", "Beep", "TurnLeft", "TurnRight"]
     
@@ -28,7 +18,6 @@ class  TaskManager {
     
     //Change this to the task you are testing
     static let taskNames = ["TurnLeft", "TurnRight"]
->>>>>>> origin/master
     
     static var totalTasksDone = 0
     static var runMode = "count"
